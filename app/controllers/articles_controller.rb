7aac4fcf169139823bfd42ce06f13f6716ed1e94
patrick bikomagu <pmuhizi1@gmail.com>
2019-05-22 10:17:55 +0000

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,79 +1,56 @@
 class ArticlesController < ApplicationController
-def index
-  @articles=Article.all
-  
-end
- 
- 
- def new
-   @articles=Article.new  
-     
- end
-    
-    
-    def create
-      @articles=Article.new(article_params)
-      if @articles.save
-      redirect_to new_article_path, notice: "created"
-  else
-      render 'new'
-    end
-        
-        
-    end
-    
-    
-    
-    def edit
-      @articles=Article.find(params[:id])
-        
-        
-    end
-    
-    
-    
-    def show
-      @articles=Article.find(params[:id])   
-        
-        
-        
-    end
-    
-    
-    
-    def destroy
-      @articles=Article.find(params[:id])
-      @articles.destroy
-      redirect_to articles_path, notice:"article delete"  
-        
-        
-        
-        
-    end
-    
-    def update
-      @articles = Article.find(params[:id])
-      if @articles.update(article_params)
-        redirect_to articles_path, notice:"Article was updated"
-      else
-     
-  
-        render 'edit'
-      end
-    
-    end
-    
-    
-      private
-  
-  def article_params
-    params.require(:article).permit(:title,:description)
-  
+  def index
+    @articles=Article.all
   end
   
+  def new
+    @articles=Article.new  
+     
+  end
     
     
+  def create
+    @articles=Article.new(article_params)
+    if @articles.save
+      redirect_to new_article_path, notice: "created"
+    else
+      render 'new'
+    end
+  end
+    
+  def edit
+    @articles=Article.find(params[:id])
+        
+        
+  end
+    
+  def show
+    @articles=Article.find(params[:id])   
+        
+  end
+    
+  def destroy
+    @articles=Article.find(params[:id])
+    @articles.destroy
+    redirect_to articles_path, notice:"article delete"  
+        
+  end
+    
+  def update
+    @articles = Article.find(params[:id])
+    if @articles.update(article_params)
+       redirect_to articles_path, notice:"Article was updated"
+    else
+       render 'edit'
+    end
+    
+  end
     
     
-
+  private
+  
+    def article_params
+    params.require(:article).permit(:title,:description)
+  
+    end
 end
